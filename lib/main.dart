d523babdbcbf851ddd352af17383daa4d33331da
alMohamady <al.Mohamady47@gmail.com>
2019-07-22 20:48:38 +0300

    INITH COMMIT 01

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyCalculator());
+
+
+class MyCalculator extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
+
+class Calculator extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _Calculator();
+  }
+}
+
+class _Calculator extends State<Calculator> {
+
+  String text = '0';
+  double numOne = 0;
+  double numTwo = 0;
+
+  String result = '0';
+  String finalResult = '0';
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
